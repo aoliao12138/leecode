f470fe4ed56f063a607330f4b3a94a3744e4fced
wangliao <lovelive12138@126.com>
2017-10-23 02:45:40 +0800

    nomessage

---
+++ 1two_sum.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            if target-nums[i] in nums:
+                return [i,nums.index(target-nums[i])]
+
+# wrong forgetting it should be different numbers in list like [3,2,4] 6
+# it will return [0,0]
+
+
+
+# others O(n) solution
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
